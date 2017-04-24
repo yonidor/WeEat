37c23feac79cd8fa4bc3820c9ab57b81a788bee9
Yoni Dor <yoni.dor@wework.com>
2017-04-24 14:55:54 +0300

    Add restaurant model and controller

---
+++ WeEat/app/helpers/restraurants_helper.rb
@@ -0,0 +1,2 @@
+module RestraurantsHelper
+end
---
+++ WeEat/test/controllers/restraurants_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RestraurantsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
