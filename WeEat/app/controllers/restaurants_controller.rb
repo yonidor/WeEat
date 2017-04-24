50665ee7261288a57bb074483fee20cd30ee48c2
Yoni Dor <yoni.dor@wework.com>
2017-04-24 18:06:10 +0300

    Add ability to edit a restaurant

--- WeEat/app/controllers/restaurants_controller.rb
+++ WeEat/app/controllers/restaurants_controller.rb
@@ -12,6 +12,7 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def create
@@ -25,11 +26,16 @@ class RestaurantsController < ApplicationController
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+
+    @restaurant.update(restaurant_params)
+
+    redirect_to @restaurant
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :cuisines)
+    params.require(:restaurant).permit(:name, :cuisines, :rating, :accepts10bis, :address, :max_delivery_times)
   end
 end
