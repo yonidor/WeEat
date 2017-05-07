eb545dbc89cd8832fa7e5d5a9954576694437fa7
Yoni Dor <yoni.dor@wework.com>
2017-05-07 13:56:34 +0300

    Move to postgres db

--- WeEat/db/schema.rb
+++ WeEat/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170430121153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "cuisines"
@@ -30,7 +33,8 @@ ActiveRecord::Schema.define(version: 20170430121153) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
